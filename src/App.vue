3525f43db47d882f089c6071dfb64ebb92771685
Zeniuus <jsh56son@kaist.ac.kr>
2017-11-11 22:21:03 +0900

    App.vue apply global css

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,26 @@
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #ffffff;
+    background-color: #000000;
+    padding-top: 50px;
+    height: 100%;
+    padding-bottom: 100px;
+    position: relative;
+  }
+
+  #footer {
+    position: absolute;
+    bottom: 0;
+    width: 100vw;
+  }
+
+  a:link, :visited {
+    color: #ff0000;
+  }
 </style>
