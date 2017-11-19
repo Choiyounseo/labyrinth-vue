90bc98d38e90bcca30850b60940df663d577dbfa
Zeniuus <jsh56son@kaist.ac.kr>
2017-11-20 02:44:40 +0900

    Main.vue: render history problems & stories without API call but just with user's progress

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,25 +10,20 @@
     <span>
       이전 문제 목록
     </span>
-    <div id="past_problem_list">
-      <span v-for="problem in problems">
-        <router-link :to="{ name: 'Problem', params: { number: problem.number }}"></router-link>
-      </span>
+    <div>
+      <router-link class="history_item" v-for="i in problemHistory" :to="{ name: 'Problem', params: { number: i }}">{{ i }}</router-link>
     </div>
     <br />
     <span>
       이전 스토리 목록
     </span>
     <div id="past_story_list">
-      <span v-for="story in stories">
-        <router-link :to="{ name: 'Story', params: { number: story.number }}"></router-link>
-      </span>
+      <router-link class="history_item" v-for="i in storyHistory" :to="{ name: 'Story', params: { number: i }}">{{ i }}</router-link>
     </div>
   </div>
 </template>
 
 <script>
-  import axios from 'axios';
   import { mapState, mapActions, mapGetters } from 'vuex';
 
   export default {
@@ -46,6 +41,18 @@
       ...mapGetters([
         'currProbUrl',
       ]),
+      problemHistory() {
+        if (!this.user) {
+          return [];
+        }
+        return Array(this.user.progress).fill().map((x, i) => i + 1);
+      },
+      storyHistory() {
+        if (!this.user) {
+          return [];
+        }
+        return Array(this.user.progress + 1).fill().map((x, i) => i);
+      },
     },
     methods: {
       ...mapActions([
@@ -54,20 +61,6 @@
     },
     mounted() {
       this.updateUser();
-      axios.get('/api/problems')
-        .then((res) => {
-          this.problems = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      axios.get('/api/stories')
-        .then((res) => {
-          this.stories = res.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
     },
   };
 </script>
